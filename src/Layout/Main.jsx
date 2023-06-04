25c88a53ce48d2534aac980a26e50f5b3927479c
Manik <tonysins43@gmail.com>
2023-06-04 15:24:52 +0600

    project setup install react router dom , taiwind, daisy ui etc

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>kThis is main layout</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+
+ export const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>
+    }
+])
